0399cbecfae4b4d04babd4b97418d1a97ba38502
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-21 14:31:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,6 +27,8 @@ class Song
     @@genre_count = {}
     @@genres each do |genre| 
       if @@genre_count[genre]
+        @genre_count[genre] += 1
+        end 
   end 
   def self.artist_count
   end 
