a4b76728df37b0a461197f16fa9dbca6acdd38c3
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-21 14:44:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,14 +33,7 @@ class Song
     end
     genre_count
   end
-  def self.artist_count
-    artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1 
-      else 
-        artist_count[artist] = 1 
-      end 
+  #
   end 
 #binding.pry
 end 
