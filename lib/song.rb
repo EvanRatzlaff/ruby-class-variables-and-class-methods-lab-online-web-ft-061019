caed7bc57011fc54add550c24b1ab83d88f1eaa5
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-21 14:33:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
   end 
   def self.genre_count
     @@genre_count = {}
-    @@genres each do |genre| 
+    @@genres.each do |genre| 
       if genre_count[genre]
         genre_count[genre] += 1
       else genre[count] = 1 
